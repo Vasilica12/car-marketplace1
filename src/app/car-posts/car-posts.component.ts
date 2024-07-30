a2f63170701e194f9840cb14fe25fddc6ff940cf
Vasilica Cernovschi <vasilica.cernovschi@assist.ro>
2024-07-30 18:07:29 +0300

    card is added

--- src/app/car-posts/car-posts.component.ts
+++ src/app/car-posts/car-posts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PostCreateComponent } from './post-create/post-create.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @Component({
   selector: 'app-car-posts',
   standalone: true,
-  imports: [PostCreateComponent],
+  imports: [PostCreateComponent, PostListComponent],
   templateUrl: './car-posts.component.html',
   styleUrl: './car-posts.component.css'
 })
