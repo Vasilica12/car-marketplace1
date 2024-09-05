570294aa06268a314d6191d4da2afb266dc9cff3
Vasilica Cernovschi <vasilica.cernovschi@assist.ro>
2024-09-05 23:59:04 +0300

    filterAdded

--- src/app/car-posts/post-list/car-list.component.ts
+++ src/app/car-posts/post-list/car-list.component.ts
@@ -9,6 +9,8 @@ import { Subscription } from 'rxjs';
 import { RouterModule } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthService } from '../../auth/auth.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
 
 @Component({
   selector: 'app-car-list',
@@ -19,18 +21,21 @@ import { AuthService } from '../../auth/auth.service';
     CommonModule, 
     RouterModule, 
     MatProgressSpinnerModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.css'
 })
 export class CarListComponent implements OnInit, OnDestroy {
   cars : Car [] = [];
+  filteredCars: Car[] = [];
   private authListenerSubs!: Subscription;
   userAuth = false;
   isLoading = false;
   totalCars = 0;
-  carsPerPage = 2;
+  carsPerPage = 5;
   currentPage = 1;  
   userId!: string;
   pageSizeOptions = [1, 2, 5, 10];
@@ -52,6 +57,7 @@ export class CarListComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.totalCars = carsData.carsCount;
         this.cars = carsData.cars;
+        this.filteredCars = [...this.cars];
     });
   }
 
@@ -67,6 +73,11 @@ export class CarListComponent implements OnInit, OnDestroy {
     this.carService.deleteCars(carId).subscribe(() => {
       this.carService.getCars(this.carsPerPage, this.currentPage);
     });
+  }
+
+  onFilterChange(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value.toLowerCase();
+    this.filteredCars = this.cars.filter(car => car.model.toLowerCase().includes(filterValue));
   }
 
   ngOnDestroy() {
